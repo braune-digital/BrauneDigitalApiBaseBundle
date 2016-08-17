80a6b9ce144395c7819c691ef8a223877e9f7b7b
Patrick Rathje <PatrickRathje@web.de>
2016-08-17 22:38:00 +0200

    Update AbstractApiType.php

--- Form/Type/AbstractApiType.php
+++ Form/Type/AbstractApiType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AbstractApiType extends AbstractType {
+abstract class AbstractApiType extends AbstractType {
 
     private static $apiRequestHandler;
 
