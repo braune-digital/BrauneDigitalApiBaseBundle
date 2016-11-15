b60a58a04bc5397548cff671df83d6b27455f675
hbraune <henrik@braune.org>
2016-11-15 17:06:42 +0100

    Added alternative find method

--- Security/ApiKeyUserProvider.php
+++ Security/ApiKeyUserProvider.php
@@ -3,15 +3,50 @@
 namespace BrauneDigital\ApiBaseBundle\Security;
 
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\NoResultException;
 use FOS\UserBundle\Model\UserManagerInterface;
 use FOS\UserBundle\Security\UserProvider;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class ApiKeyUserProvider extends UserProvider
 {
 
+	/**
+	 * @var ContainerInterface
+	 */
+	protected $container;
+
     public function getUserForApiKey($apiKey)
     {
-        return $this->userManager->findUserBy(array('token' => $apiKey));
+
+		$features = $this->container->getParameter('braune_digital_api_base.features');
+		if ($features['use_token_relation']) {
+			$qb = $this->container->get('doctrine')->getRepository('VSFFahrschuleFlorinBaseBundle:User')->createQueryBuilder('u');
+			$qb
+				->leftJoin('u.tokens', 't')
+				->where($qb->expr()->eq('t.token', $qb->expr()->literal($apiKey)))
+				->andWhere($qb->expr()->gte('t.expiresAt', ':now'))
+				->setParameter('now', new \DateTime('-5 second'), \Doctrine\DBAL\Types\Type::DATETIME)
+				->setMaxResults(1)
+			;
+			try {
+				return $qb->getQuery()->getSingleResult();
+			} catch (NoResultException $e) {
+				return false;
+			}
+		} else {
+        	return $this->userManager->findUserBy(array('token' => $apiKey));
+		}
     }
+
+	/**
+	 * @param ContainerInterface $container
+	 */
+	public function setContainer($container)
+	{
+		$this->container = $container;
+	}
+
+
 }
